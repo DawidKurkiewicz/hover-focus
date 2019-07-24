b694f7257762234dc9698eccab2e065110504346
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-07-24 09:26:18 +0200

    code refactor

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const triggers = document.querySelectorAll("a")
-const highlight = document.createElement("span")
-highlight.classList.add("highlight")
-document.body.append(highlight)
-
-function highlightLink(){
-   const linkCoords = this.getBoundingClientRect()
-   highlight.style.width=`${linkCoords.width}px`
-   highlight.style.height=`${linkCoords.height}px`
-
-}
-
-triggers.forEach(a => a.addEventListener("mouseenter", highlightLink))
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
+highlight.classList.add("highlight");
+document.body.append(highlight);
+function highlightLink() {
+    const linkCoords = this.getBoundingClientRect();
+    const coords = {
+      width: linkCoords.width,
+      height: linkCoords.height,
+      top: linkCoords.top + window.scrollY,
+      left: linkCoords.left + window.scrollX
+    };
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+  }
+triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
